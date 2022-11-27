58afd255f465bbd5d18d2170f985805c671d8595
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2022-11-27 22:01:06 +0700

    feat: add CRUD for customers

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const user = require('./user')
 const warehouse = require('./warehouse')
+const customer = require('./customer')
 
 const router = express.Router()
 
 router.use('/users', user)
 router.use('/warehouses', warehouse)
+router.use('/customers', customer)
 
 module.exports = router
