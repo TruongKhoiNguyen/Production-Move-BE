caee49153a265b7e15da90990d4fda1e95a72bbf
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2022-12-23 16:11:37 +0700

    feat: add get warehouses by user id

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const userController = require('../controllers/users_controller')
+const warehousesController = require('../controllers/warehouses_controller')
 
 const router = express.Router()
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.get('/', userController.getAll)
+router.get('/:userId/warehouses', warehousesController.getWarehousesByUser)
 
 module.exports = router
