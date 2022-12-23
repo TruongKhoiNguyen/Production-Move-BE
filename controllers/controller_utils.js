d4259d9c774b2e32ff53c67de9b1f1ed6670514d
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2022-12-23 20:59:37 +0700

    feat: get all warehouses

--- controllers/controller_utils.js
+++ controllers/controller_utils.js
@@ -7,6 +7,18 @@ class ControllerUtil {
 
         return result
     }
+
+    static pagination(req) {
+        let limit = 10
+        let offset = 0
+
+        try {
+            limit = parseInt(req.query.limit) || limit
+            offset = (parseInt(req.query.page) - 1) * limit || offset
+        } catch (err) { }
+
+        return { limit: limit, offset: offset }
+    }
 }
 
 module.exports = ControllerUtil
--- controllers/warehouses_controller.js
+++ controllers/warehouses_controller.js
@@ -1,5 +1,6 @@
-const { User } = require('../models/models_manager').models
+const { User, Warehouse } = require('../models/models_manager').models
 const Response = require('../views/response')
+const ControllerUtil = require('./controller_utils')
 
 const getWarehousesByUser = async (req, res) => {
     const { userId } = req.params
@@ -17,6 +18,30 @@ const getWarehousesByUser = async (req, res) => {
     Response.ok(res, { data: warehouses })
 }
 
+const getAll = async (req, res) => {
+
+    const { limit, offset } = ControllerUtil.pagination(req)
+    const { id, role } = req.user
+
+    try {
+        if (role === 'executive') {
+            const result = await Warehouse.findAll({ limit: limit, offset: offset })
+
+            return Response.ok(res, { data: formatResult(result) })
+        }
+
+        const result = await Warehouse.findAll({ limit: limit, offset: offset, where: { user_id: id } })
+
+        return Response.ok(res, { data: formatResult(result) })
+
+    } catch (err) {
+        return Response.internalServerError(res, err.message)
+    }
+}
+
+const formatResult = (result) => result.map(warehouse => ({ id: warehouse.id, location: warehouse.location }))
+
 module.exports = {
-    getWarehousesByUser
+    getWarehousesByUser,
+    getAll
 }
