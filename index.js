e020b642434d513adfa59af8a5de9ffa314160b6
Hoàng Quý <78021023+quysterben@users.noreply.github.com>
2022-12-25 18:41:55 +0700

    feat: enable cors request

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const passport = require('passport')
+const express = require("express");
+const dotenv = require("dotenv");
+const passport = require("passport");
+var cors = require("cors");
 
-const { loginCheck } = require('./auth/passport')
-loginCheck(passport)
+const { loginCheck } = require("./auth/passport");
+loginCheck(passport);
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
+
+// enable cors all request
+app.use(cors());
 
 // middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(passport.initialize())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
 // routing
-app.use('/api/v1', require('./routes/api'))
+app.use("/api/v1", require("./routes/api"));
 
 // error handler
 app.use((err, req, res, next) => {
-    console.log(err.stack)
-    res.status(500).json({ error: err })
-})
+    console.log(err.stack);
+    res.status(500).json({ error: err });
+});
 
-const PORT = process.env.PORT || 4111
-app.listen(PORT, () => { console.log('Server start on port ' + PORT) })
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, () => {
+    console.log("Server start on port " + PORT);
+});
