a99ea4c53c016c1fa01afff0f585d8cc91e76ba4
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2022-12-27 15:08:00 +0700

    refactor: return broken product to factory

--- routes/warranty.js
+++ routes/warranty.js
@@ -5,5 +5,6 @@ const { checkRole } = require('../auth/project')
 const router = express.Router()
 
 router.post('/send', checkRole(['warranty']), warrantyController.send)
+router.post('/return', checkRole(['warranty']), warrantyController.returnToFactory)
 
 module.exports = router
