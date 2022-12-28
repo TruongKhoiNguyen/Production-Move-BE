73732d644e122bcf1453f9e45ead9d6203a46995
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2022-12-28 07:28:35 +0700

    feat: logistics sent and inbox

--- routes/logistics.js
+++ routes/logistics.js
@@ -7,6 +7,7 @@ const router = express.Router()
 router.post('/:delivery_id', checkRole(['production', 'distribution', 'warranty']), logisticsController.receive)
 router.post('/', checkRole(['production', 'distribution', 'warranty']), logisticsController.send)
 
-router.get('/', logisticsController.getAll)
+router.get('/inbox', logisticsController.getInbox)
+router.get('/sent', logisticsController.getSent)
 
 module.exports = router
